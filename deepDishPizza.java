e8c2f4cce163f4b239358e1fb3b3a049436bdd3f
sammym620 <sammymukkamala@gmail.com>
2024-10-25 21:53:02 -0700

    Create deepDishPizza.java

---
+++ deepDishPizza.java
@@ -0,0 +1,13 @@
+// Sammy Mukkamala
+// Period: 5
+// Date: 10/24/24
+
+// Class representing a deep dish pizza, extending the Pizza class
+public class deepDishPizza extends Pizza 
+{
+    // Constructor that sets ingredient and uses "deep dish pizza" as the name
+    public deepDishPizza(String ingredientIn) 
+    {
+        super(ingredientIn, "deep dish pizza");
+    }
+}
