70747a8bf730a9f7b8c243c74c6e5caf9a153703
sammym620 <sammymukkamala@gmail.com>
2024-10-25 21:46:53 -0700

    Create deepDishPizza3.java

---
+++ deepDishPizza3.java
@@ -0,0 +1,5 @@
+public class deepDishPizza3 extends Pizza3 {
+    public deepDishPizza3(String ingredientIn, int numPieces, double costPerPiece) {
+        super(ingredientIn, "deep dish pizza", numPieces, costPerPiece);
+    }
+}
