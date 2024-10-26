503417775f7173cda509f844a42b60c4c04398df
sammym620 <sammymukkamala@gmail.com>
2024-10-25 21:54:09 -0700

    Update Food3.java

--- Food3.java
+++ Food3.java
@@ -3,25 +3,29 @@
 // Date: 10/24/24
 
 // Base class representing a general food item
-public class Food3 {
+public class Food3 
+{
     protected String str;  // Description string for the food item
     protected int numPieces;  // Number of pieces of the food item
     protected double costPerPiece;  // Cost per piece of the food item
 
     // Default constructor
-    public Food3() {
+    public Food3() 
+    {
         str = "";  // Initialize empty string
     }
 
     // Parameterized constructor to initialize food item with preparation method, ingredient, name, number of pieces, and cost per piece
-    public Food3(String prepMethod, String ingredient, String name, int numPieces, double costPerPiece) {
+    public Food3(String prepMethod, String ingredient, String name, int numPieces, double costPerPiece) 
+    {
         this.str = "At the sale: " + name + " " + prepMethod + " with " + ingredient;
         this.numPieces = numPieces;
         this.costPerPiece = costPerPiece;
     }
 
     // Method to print the sale information for the food item
-    public void printForSale() {
+    public void printForSale() 
+    {
         // Format the output string to show food item details and calculated total cost
         String formattedOutput = String.format("%s - Pieces: %d, Cost per piece: $%.2f, Total: $%.2f",
                 str, numPieces, costPerPiece, numPieces * costPerPiece);
