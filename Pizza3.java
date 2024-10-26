9770be2b7d2754addc7e1a0e12f226dc43311b17
sammym620 <sammymukkamala@gmail.com>
2024-10-25 21:54:41 -0700

    Update Pizza3.java

--- Pizza3.java
+++ Pizza3.java
@@ -3,15 +3,18 @@
 // Date: 10/24/24
 
 // Class representing a Pizza, inheriting from Food3
-public class Pizza3 extends Food3 {
+public class Pizza3 extends Food3 
+{
     // Constructor that accepts only ingredient, number of pieces, and cost per piece
-    public Pizza3(String ingredientIn, int numPieces, double costPerPiece) {
+    public Pizza3(String ingredientIn, int numPieces, double costPerPiece) 
+    {
         // Call superclass constructor with default name "pizza"
         super("baked", ingredientIn, "pizza", numPieces, costPerPiece);
     }
 
     // Constructor that accepts ingredient, custom name, number of pieces, and cost per piece
-    public Pizza3(String ingredientIn, String nameIn, int numPieces, double costPerPiece) {
+    public Pizza3(String ingredientIn, String nameIn, int numPieces, double costPerPiece) 
+    {
         // Call superclass constructor with provided name
         super("baked", ingredientIn, nameIn, numPieces, costPerPiece);
     }
