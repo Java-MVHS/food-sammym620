8def371a048205065f8293f5611049d64efa639d
sammym620 <sammymukkamala@gmail.com>
2024-10-25 21:55:02 -0700

    Update deepDishPizza3.java

--- deepDishPizza3.java
+++ deepDishPizza3.java
@@ -1,10 +1,13 @@
-//Sammy Mukkamala
-//Period 5
-//10/24/24
+// Sammy Mukkamala
+// Period: 5
+// Date: 10/24/24
+
 // Class representing a deep dish pizza, extending the Pizza3 class
-public class deepDishPizza3 extends Pizza3 {
+public class deepDishPizza3 extends Pizza3 
+{
     // Constructor that sets ingredient, number of pieces, and cost per piece, with default name "deep dish pizza"
-    public deepDishPizza3(String ingredientIn, int numPieces, double costPerPiece) {
+    public deepDishPizza3(String ingredientIn, int numPieces, double costPerPiece) 
+    {
         // Call superclass constructor with specified name and properties
         super(ingredientIn, "deep dish pizza", numPieces, costPerPiece);
     }
